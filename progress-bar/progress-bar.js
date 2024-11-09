0102cb91fa6986a99f7dd98f16f587a991863ff7
Gilmar Ferrari Filho <gilmarferrarifilho15@mail.com>
2024-11-09 15:24:02 -0300

    Updated techs page.

---
+++ progress-bar/progress-bar.js
@@ -0,0 +1,45 @@
+class ProgressBar extends HTMLElement {
+    static get observedAttributes() {
+        return ['progress'];
+    }
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const script = document.currentScript || (function () {
+            const scripts = document.getElementsByTagName('script');
+            return scripts[scripts.length - 1];
+        })();
+
+        const scriptDir = script.src.substring(0, script.src.lastIndexOf('/'));
+        
+        this.progress = Math.min(Math.max(this.progress, 0), 100);
+        var dotsFilled = Math.round(this.progress / 20) - 1;
+
+        const listItems = Array.from({ length: 5 }, (_, index) =>
+            `<li class="${index === dotsFilled ? 'break' : ''}"></li>`
+        ).join('');
+
+        this.innerHTML = `
+            <head>
+                <link rel="stylesheet" type="text/css" href="${scriptDir}/progress-bar.css">
+            </head>
+            <ul class="progress-bar">
+                ${listItems}
+            </ul>
+            <label class="progress-percentage">${this.progress}%</label>
+        `;
+    }
+
+    get progress() {
+        return this.getAttribute('progress');
+    }
+
+    set progress(value) {
+        this.setAttribute('progress', value);
+    }
+}
+
+customElements.define('progress-bar', ProgressBar);
